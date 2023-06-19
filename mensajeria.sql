8d438b1070d82b15a6230ac95ab4458a6914ade1
Pedro Moldenhauer <molden14@gmail.com>
2023-06-19 10:00:58 +0200

    Actualizada base de datos

--- mensajeria.sql
+++ mensajeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-02-2023 a las 13:35:25
+-- Tiempo de generación: 19-06-2023 a las 10:00:11
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -32,19 +32,39 @@ CREATE TABLE `mensajes` (
   `remitente` varchar(20) NOT NULL,
   `destinatario` varchar(20) NOT NULL,
   `asunto` varchar(100) NOT NULL,
-  `cuerpo` text NOT NULL
+  `cuerpo` text NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 3
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `mensajes`
 --
 
-INSERT INTO `mensajes` (`id`, `remitente`, `destinatario`, `asunto`, `cuerpo`) VALUES
-(1, 'Pepe', 'ana', 'Cumpleaños', 'Hola ana te invito a mi fiesta de cumpleaños, un saludo.'),
-(2, 'ana', 'Pepe', 'Felicidades!', 'Felicidades, alli estare, un saludo.'),
-(22, 'ana', 'pepe', 'prueba', 'pruebasa'),
-(29, 'ana', 'juan', 'prueba', 'prueba'),
-(32, 'ana', 'pepe', 'pruebaaa', 'vgwasfvr');
+INSERT INTO `mensajes` (`id`, `remitente`, `destinatario`, `asunto`, `cuerpo`, `estado`) VALUES
+(1, 'Pepe', 'ana', 'Cumpleaños', 'Hola ana te invito a mi fiesta de cumpleaños, un saludo.', 3),
+(2, 'ana', 'Pepe', 'Felicidades!', 'Felicidades, alli estare, un saludo.', 3),
+(22, 'ana', 'pepe', 'prueba', 'pruebasa', 3),
+(29, 'ana', 'juan', 'prueba', 'prueba', 3),
+(32, 'ana', 'pepe', 'pruebaaa', 'vgwasfvr', 3),
+(36, 'pepe', 'juan', 'test3', 'rgaaqgav\n', 2),
+(38, 'juan', 'pepe', 'testjuan', 'test de mensaje', 3),
+(39, 'pepe', 'juan', 'Prueba mensaje largo', '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 3),
+(40, 'pepe', 'ana', 'test de pulsar boton', 'test de pulsar el botonsito.', 3),
+(41, 'pepe', 'ana', '', '', 1),
+(42, 'pepe', 'ana', 'test 2 de pulsar el botoncito', 'fqeafq', 3),
+(43, 'pepe', 'ana', 'test 3 de pulsar el botonsito', 'dacqacfqace', 3),
+(44, 'pepe', 'ana', '', '', 1),
+(45, 'pepe', 'ana', 'prueba botoncillo 4', 'fqfe', 1),
+(46, 'pepe', 'ana', 'testt', 'SVG<<SW', 1),
+(47, 'pepe', 'ana', 'eqga', 'gaeqeg', 1),
+(48, 'pepe', 'ana', 'gqeag4', 'qg4g4', 1),
+(49, 'pepe', 'ana', 'rfawgr', 'rfqra', 1),
+(50, 'pepe', 'ana', 'efqf', 'gsrgw', 1),
+(51, 'pepe', 'ana', 'faszva', '', 1),
+(52, 'pepe', 'ana', 'vavqaegqgarv', '', 1),
+(53, 'pepe', 'ana', 'aqrvaqgrg', '', 1),
+(54, 'pepe', 'ana', 'vqaegaav', '', 1),
+(55, 'pepe', 'ana', 'vqaearqrb', '', 1);
 
 -- --------------------------------------------------------
 
@@ -85,7 +105,7 @@ ALTER TABLE `mensajes`
 -- AUTO_INCREMENT de la tabla `mensajes`
 --
 ALTER TABLE `mensajes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
